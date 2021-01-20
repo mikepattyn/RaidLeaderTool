2128d62e962be31a63c1a5631f7cba6589a80cfa
Mike <mike@givtapp.net>
2021-01-20 13:33:56 +0100

    implement lib stub addon

--- resources/general/buff-check.lua
+++ resources/general/buff-check.lua
@@ -1,4 +1,5 @@
 function InitiateBuffCheck()
+    -- frame for buff checker
     local frame = CreateFrame("Frame", "BuffCheckerFrame", UIParent, "BackdropTemplate")
     frame:SetWidth(350)
     frame:SetHeight(150)
@@ -16,7 +17,7 @@ function InitiateBuffCheck()
     })
     frame:SetBackdropColor(0, 0, 0, 0.8)
     frame:SetBackdropBorderColor(0.1, 0.1, 0.1, 0.8)
-    -- (3)
+
     frame:EnableMouse(true)
     frame:SetMovable(true)
     frame:RegisterForDrag("LeftButton")
@@ -24,19 +25,37 @@ function InitiateBuffCheck()
     frame:SetScript("OnDragStop", frame.StopMovingOrSizing)
     frame:SetScript("OnHide", frame.StopMovingOrSizing)
 
-    -- (4)
+    -- button to close the frame
     local close = CreateFrame("Button", "BuffCheckerCloseButton", frame, "UIPanelCloseButton")
     close:SetPoint("TOPRIGHT", frame, "TOPRIGHT")
     close:SetScript("OnClick", function()
         frame:Hide()
     end)
 
+    -- getting the groupmember count
     local groupMemberCount = GetNumGroupMembers("LE_PARTY_CATEGORY_INSTANCE")
 
-    -- (5)
-    local text = frame:CreateFontString(nil, "ARTWORK", "GameFontHighlight")
-    text:SetPoint("CENTER")
-    text:SetText(groupMemberCount)
+    local coldata = {{
+        ["name"] = "Character",
+        ["width"] = 80
+    }, {
+        ["name"] = "DBM",
+        ["width"] = 40
+    }, {
+        ["name"] = "Food",
+        ["width"] = 40
+    }, {
+        ["name"] = "Flask",
+        ["width"] = 40
+    }};
+
+    local data = {{ "Sachmo", "OK", "true", "false"}, {"Josua", "false", "true", "false"}};
+
+    -- create a table here and add to frame
+    local tableStub = LibStub("ScrollingTable")
+    local table = tableStub:CreateST(coldata, 2, 20, nil, frame)
+    table.frame:SetPoint("TOPLEFT", frame ,"TOPLEFT",10,-20)
+    table:SetData(data, true)
 
     return frame
 end
