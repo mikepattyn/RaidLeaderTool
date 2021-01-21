304f5f9ace89255bc3cf7063a06a9a830db6faaf
Mike <mike@givtapp.net>
2021-01-21 20:55:58 +0100

    make buff check frame nicer and fit to the table

--- resources/general/buff-check.lua
+++ resources/general/buff-check.lua
@@ -1,8 +1,17 @@
+function tablelength(T)
+    local count = 0
+    for _ in pairs(T) do count = count + 1 end
+    return count
+end
+
 function InitiateBuffCheck()
     -- frame for buff checker
     local frame = CreateFrame("Frame", "BuffCheckerFrame", UIParent, "BackdropTemplate")
-    frame:SetWidth(350)
-    frame:SetHeight(150)
+    frame:SetWidth(214)
+    local customHeight = (GetNumGroupMembers("LE_PARTY_CATEGORY_INSTANCE") + 1) * 20 + 80
+
+    frame:SetHeight(customHeight)
+
     frame:SetPoint("CENTER")
     frame:SetBackdrop({
         bgFile = "Interface\\ChatFrame\\ChatFrameBackground",
@@ -32,45 +41,69 @@ function InitiateBuffCheck()
         frame:Hide()
     end)
 
+    frame.text = frame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+    frame.text:SetPoint("TOP", frame, "TOP", 0, -5)
+    frame.text:SetText("Buff checker")
+
     -- getting the groupmember count
     local groupMemberCount = GetNumGroupMembers("LE_PARTY_CATEGORY_INSTANCE")
 
-    local tableData = {}
+    local tableData = {{"Placeholder", "X", "X", "X"}}
 
     local homePlayers = GetHomePartyInfo()
 
-    for i=1,groupMemberCount,1 do
-        local raidMember = homePlayers[i]
-        tableData.insert({raidMember.name, "false", "false", "false"})
+    if IsInRaid() then
+        for i = 1, GetNumGroupMembers() do
+            -- print("In raid")
+            table.insert(tableData, {(UnitName('raid' .. i)), "X", "X"})
+            -- print((UnitName('raid' .. i)))
+        end
+    elseif IsInGroup() then
+        for i = 1, GetNumGroupMembers() do
+            -- print("In group")
+            table.insert(tableData, {(UnitName('party' .. i)), "X", "X"})
+            -- print((UnitName('party' .. i)))
+        end
+    else
+        print("I am not in a group")
     end
-   
+
     local coldata = {{
         ["name"] = "Character",
         ["width"] = 80
     }, {
-        ["name"] = "DBM",
-        ["width"] = 40
-    }, {
         ["name"] = "Food",
-        ["width"] = 40
+        ["width"] = 40,
+        ["align"] = "CENTER",
+        ["DoCellUpdate"] = BuffCheckFrameSetCellFood
     }, {
         ["name"] = "Flask",
-        ["width"] = 40
+        ["width"] = 40,
+        ["align"] = "CENTER",
+        ["DoCellUpdate"] = BuffCheckFrameSetCellFlask
     }};
 
     -- create a table here and add to frame
     local tableStub = LibStub("ScrollingTable")
-    local table = tableStub:CreateST(coldata, groupMemberCount, 20, nil, frame)
-    table.frame:SetPoint("TOPLEFT", frame ,"TOPLEFT",10,-20)
+    local table = tableStub:CreateST(coldata, tablelength(tableData), 20, nil, frame)
+    table.frame:SetPoint("TOPLEFT", frame, "TOPLEFT", 11, -50)
     table:SetData(tableData, true)
 
-    local text = frame:CreateFontString(nil, "ARTWORK", "GameFontHighlight")
-    text:SetPoint("BOTTOMRIGHT")
-    text:SetText(homePlayers)
-
-    local text = frame:CreateFontString(nil, "ARTWORK", "GameFontHighlight")
-    text:SetPoint("BOTTOMLEFT")
-    text:SetText(groupMemberCount)
-
     return frame
+end
+
+function BuffCheckFrameSetCellFood(rowFrame, cellFrame, data, cols, row, realrow, column, fShow, table, ...)
+    local myCheckButton = CreateFrame("CheckButton", "CheckBoxUserFood", cellFrame, "ChatConfigCheckButtonTemplate");
+    myCheckButton:SetPoint("CENTER");
+    myCheckButton:SetChecked(true)
+    myCheckButton:Disable()
+    myCheckButton:SetDisabledCheckedTexture(myCheckButton:GetCheckedTexture())
+end
+
+function BuffCheckFrameSetCellFlask(rowFrame, cellFrame, data, cols, row, realrow, column, fShow, table, ...)
+    local myCheckButton = CreateFrame("CheckButton", "CheckBoxUserFlask", cellFrame, "ChatConfigCheckButtonTemplate");
+    myCheckButton:SetPoint("CENTER");
+    myCheckButton:SetChecked(true)
+    myCheckButton:Disable()
+    myCheckButton:SetDisabledCheckedTexture(myCheckButton:GetCheckedTexture())
 end
